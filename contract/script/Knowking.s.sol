18545423a8cff917941a2be74c56b227b519f740
Ray Huang <bjhuang@cs.nycu.edu.tw>
2025-04-04 23:35:56 +0800

    feat: add deploy contracts script

--- contract/script/Knowking.s.sol
+++ contract/script/Knowking.s.sol
@@ -6,13 +6,18 @@ import {KnowledgeKingToken} from "../src/Token.sol";
 import {KnowledgeKingGame} from "../src/Knowking.sol";
 
 contract CounterScript is Script {
-    KnowledgeKingToken token;
-    KnowledgeKingGame game;
-
-    function setUp() public {}
-
     function run() public {
         vm.startBroadcast();
+
+        // Deploy the KnowledgeKingToken contract
+        KnowledgeKingToken token = new KnowledgeKingToken();
+        console.log("KnowledgeKingToken deployed at:", address(token));
+
+        // Deploy the KnowledgeKingGame contract
+        KnowledgeKingGame game = new KnowledgeKingGame(token);
+        console.log("KnowledgeKingGame deployed at:", address(game));
+
+        token.approve(address(game), type(uint256).max);
 
         vm.stopBroadcast();
     }
