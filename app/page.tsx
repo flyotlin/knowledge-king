62c5d10f58e9e4c4e17c83fffb16e45ebb66307c
flyotlin <flyotlin@gmail.com>
2025-04-04 21:30:55 +0800

    feat: add gaming componment

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { PayBlock } from "@/components/Pay";
 import { SignIn } from "@/components/SignIn";
 import { VerifyBlock } from "@/components/Verify";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <SignIn />
-      <VerifyBlock />
-      <PayBlock />
-    </main>
+
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-50 to-slate-100 p-4">
+      <div>Knowledge King</div>
+      <div>Test your knowledge with 10 random questions</div>
+      <div>Answer at least 8 questions correctly to become the Knowledge King!</div>
+      <Link href="/game">
+        <button className="btn">Start Game</button>
+      </Link>
+    </div>
   );
 }
